5820bf3f622484fabe0e62325ae8881da530c651
HYLLI <allaouainformatique@gmail.com>
2023-03-21 12:52:44 +0100

    create a header section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../../components/Header'
 
 
 
@@ -11,7 +12,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello Allaoua! Welcom</h1>
+      {/*Header*/ }
+      <Header/>
+
+      {/*Body*/ }
+
+      {/*Footer*/ }
+
     </div>
   )
 }
